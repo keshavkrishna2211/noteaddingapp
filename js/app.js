8af1f1aa3467766a4a19c0724d00bc24f4433c59
Keshav <keshavkrishna2211@gmail.com>
2021-04-16 13:28:55 +0530

    title value

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ addBtn.addEventListener('click', function (e) {
   notesObj.push(myObj);
   localStorage.setItem('notes', JSON.stringify(notesObj));
   addTxt = '';
+  addTitle = '';
 
   showNotes();
 });
@@ -34,7 +35,7 @@ function showNotes() {
     html += `<div class=" noteCard my-2 mx-2 card" style="width: 18rem;">
         
     <div class="card-body">
-      <h5 class="card-title">Note ${element.title}</h5>
+      <h5 class="card-title">${element.title}</h5>
       <p class="card-text">${element.text}</p>
       <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
     </div>
